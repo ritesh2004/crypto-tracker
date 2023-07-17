e91fbf969177c273e72ddcd2b5cef25fbb1d3e77
Ritesh Pramanik <pramanikritesh907@gmail.com>
2023-07-17 14:51:59 +0530

    initializing repo

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Banner from '../components/Banner/Banner';
+import CoinTable from '../components/CoinTable';
+import Footer from '../components/Footer';
+
+function Homepage() {
+  return (
+    <>
+    <Banner/>
+    <CoinTable/>
+    <Footer/>
+    </>
+  )
+}
+
+export default Homepage
